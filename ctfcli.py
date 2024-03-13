0df68bf9815aebba1db9f7e37e85312429a51b37
Bemyi <benfrecce2012@gmail.com>
2024-03-13 15:05:50 -0300

    Se pueden enviar varias dificultades a cargar

--- ctfcli.py
+++ ctfcli.py
@@ -15,9 +15,21 @@ def read_challenge(directory):
 
 if len(sys.argv) > 2:
     action = sys.argv[1]
-    if action in ["install", "update"]:
+    difficulties = ["basico", "intermedio", "avanzado"]
+    actions = ["instalar", "actualizar"]
+
+    if action in actions:
         base_dir = sys.argv[2] 
-        difficulty = sys.argv[3] if len(sys.argv) > 3 else None
+        difficulty_arg = sys.argv[3] if len(sys.argv) > 3 else None
+        if difficulty_arg is None:
+            difficulties_to_process = difficulties
+        else:
+            difficulties_to_process = difficulty_arg.split(",")
+
+        for difficulty in difficulties_to_process:
+            if difficulty.strip() not in difficulties:
+                print("Error: las opciones de dificultades son 'basico' 'intermedio' 'avanzado'")
+                sys.exit(1)
 
         directories = get_dirs(base_dir)
 
@@ -25,17 +37,13 @@ if len(sys.argv) > 2:
             challenge_data = read_challenge(os.path.join(base_dir, directory))
             tags = [tag.lower() for tag in challenge_data.get("tags", [])]
 
-            if difficulty is None or difficulty in tags:
-                if action == "install":
+            if not difficulties_to_process or any(difficulty in tags for difficulty in difficulties_to_process):
+                if action == "instalar":
                     command = f'ctf challenge install "{os.path.join(base_dir, directory)}"'
                 else:
                     command = f'ctf challenge sync "{os.path.join(base_dir, directory)}"'
                 os.system(command)
-            else:
-                print("Error: las opciones de dificultades son 'basico' 'intermedio 'avanzado'")
-                break
     else:
-        print("Comando no reconocido. Uso: python3 ctfcli.py [install|update] [base_directory] [difficulty]")
+        print("Comando no reconocido. Uso: python3 ctfcli.py instalar|actualizar directorio_categoria dificultad1,dificultad2")
 else:
-    print("Uso: python3 ctfcli.py install|update [base_directory] [difficulty]")
-
+    print("Uso: python3 ctfcli.py instalar|actualizar directorio_categoria dificultad1,dificultad2")
